c3134f33c398bb3181b2c11c3cf2aa5aa5d7300a
Olzzhas <olzhasayato@gmail.com>
2022-04-08 02:23:49 +0600

    indexa fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index() {
-        return view('index');
+    public function indexa() {
+        return view('indexa');
     }
 
     public function kingdoms() {
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [MainController::class, 'index']);
+Route::get('/', [MainController::class, 'indexa']);
 
 Route::get('/kingdoms', [MainController::class, 'kingdoms']);
