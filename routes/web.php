d7d8d1c20fc3943a2437f27b16750aaa86adfd4d
Olzzhas <olzhasayato@gmail.com>
2022-04-08 01:48:56 +0600

    fix MainController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/','MainController@index');
+//Route::get('/','MainController@index');
 
+
+Route::get('/', [MainController::class, 'index']);
